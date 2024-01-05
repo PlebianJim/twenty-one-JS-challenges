8bec6960901973b0814ebb1b33eec2e5c72adbdf
PlebianJim <108274011+PlebianJim@users.noreply.github.com>
2024-01-05 12:42:35 +0000

    completed all aspects of task

--- main.js
+++ main.js
@@ -1,8 +1,66 @@
-//Print message to the console
-console.log("Hello Balls!")
+class BallTask {
+  constructor() {
+    this.ballElement = document.getElementById("ball");
+    this.scaleValue = this.generateRandomScale();
+    this.addEventListeners();
+  }
 
-//Get the canvas element that is contained in the HTML file
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+  generateRandomScale() {
+    return Math.random() * 5;
+  }
 
-//Set the canvas dimensions to the size of the window
+  changeColor() {
+    this.ballElement.onmouseover = () => {
+      this.ballElement.style.backgroundColor =
+        "#" + Math.floor(Math.random() * 16777215).toString(16);
+    };
+  }
+
+  changeColorAgain() {
+    this.ballElement.onmouseout = () => {
+      this.ballElement.style.backgroundColor =
+        "#" + Math.floor(Math.random() * 16777215).toString(16);
+    };
+  }
+
+  increaseSize() {
+    this.ballElement.onmouseover = () => {
+      this.scaleValue = this.generateRandomScale();
+      this.ballElement.style.transform = `scale(${this.scaleValue})`;
+    };
+  }
+
+  decreaseSize() {
+    this.ballElement.onmouseleave = () => {
+      this.ballElement.style.transform = `scale(1)`;
+      this.randomPosition();
+    };
+  }
+
+  randomPosition() {
+    // Generate random x and y values
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+
+    // Clip x to be within viewport
+    const xMax = window.innerWidth - ball.offsetWidth;
+    const xClipped = Math.min(Math.max(x, 0), xMax);
+
+    // Clip y to be within viewport
+    const yMax = window.innerHeight - ball.offsetHeight;
+    const yClipped = Math.min(Math.max(y, 0), yMax);
+
+    // Set clipped position
+    ball.style.left = `${xClipped}px`;
+    ball.style.top = `${yClipped}px`;
+  }
+
+  addEventListeners() {
+    this.changeColor();
+    this.changeColorAgain();
+    this.increaseSize();
+    this.decreaseSize();
+  }
+}
+
+const ballTask = new BallTask();
